918d8d4b4a0651ce97ca166a369be89ec6ef0c24
MeongGanas <farreluken@gmail.com>
2023-12-19 14:34:12 +0800

    latihan

---
+++ EIR/Eir/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+});
